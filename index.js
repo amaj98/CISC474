b6094c1f7901fc31413e5de29a95970429b73849
amaj98 <amaj1998@gmail.com>
2019-09-06 19:31:30 -0400

    button works

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 $(function(){
     
-    
     $("#nameform").submit(evt=>{
         evt.preventDefault();
         let name = $('#fname').val() + " " + $('#lname').val();
         $(".jumbotron").removeClass('d-none');
         $(".jumbotron").find('h1').text([...name].reverse().join(""));
+
         return false;
     })   
 });
